24ae9103cbccedf8a66d110486ce5b6e5fe17313
Agoziemax <agoziemax@yahoo.com>
2023-07-25 22:05:44 +0100

    updates

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import Part from './Part'
+import Total from './Total';
+
 
 function Content() {
+
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
   return (
     <div>
-      <p></p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
-  )
+  );
 }
 
 export default Content
