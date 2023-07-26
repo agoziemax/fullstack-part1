eedf66660072c32db35d301f5aecdc3815de6eea
Agoziemax <agoziemax@yahoo.com>
2023-07-26 01:03:00 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import './App.css';
 import Content from './Content';
 import Header from './Header';
@@ -6,11 +6,20 @@ import Header from './Header';
 const App = () => {
   const course = 'Half Stack application development';
 
+  const [counter, setCounter] = useState(0);
 
+  // setTimeout(() => setCounter(counter + 1), 1000);
+  //  console.log('rendering...', counter);
+  const handleClick = () => {
+    console.log('clicked');
+  };
   return (
     <div>
-      <Header course ={course}/>
-      <Content/>
+      <Header course={course} />
+      <Content />
+      <div>{counter}</div>
+      <button onClick={handleClick}>plus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
     </div>
   );
 };
